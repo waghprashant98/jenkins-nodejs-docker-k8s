0d09b442a5f22189866fc02654d501fa217e06e6
devopscloudworld <100210899+devopscloudworld@users.noreply.github.com>
2024-04-05 20:06:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello DevOps Cloud World  Training !!! . Welcome To DevOps session.');
+  res.send('Hello DevOps Cloud World  Training !!! . ');
 });
 
 app.listen(PORT, HOST);
